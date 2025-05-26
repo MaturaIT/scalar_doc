350c54e601f8ee1f6208d191fed2ed5aa8afa15f
Iago B Almeida <iago.borba.almeida@gmail.com>
2025-05-25 21:55:30 -0300

    fix: ajustando testes

--- tests.py
+++ tests.py
@@ -1,29 +1,27 @@
+import json
 import os
 import tempfile
 import unittest
 
 from scalar_doc import ScalarDoc
 
-MOCK_OPENAPI_DICT = {
-    "openapi": "3.0.0",
-    "info": {"title": "Sample API", "version": "1.0.0"},
-    "paths": {
-        "/hello": {
-            "get": {
-                "summary": "Say Hello",
-                "responses": {"200": {"description": "Successful Response"}},
+MOCK_OPENAPI_JSON = json.dumps(
+    {
+        "openapi": "3.0.0",
+        "info": {"title": "Sample API", "version": "1.0.0"},
+        "paths": {
+            "/hello": {
+                "get": {
+                    "summary": "Say Hello",
+                    "responses": {"200": {"description": "Successful Response"}},
+                }
             }
-        }
-    },
-}
+        },
+    }
+)
 
 
 class TestScalarDoc(unittest.TestCase):
-    def test_scalar_doc_from_dict(self):
-        doc = ScalarDoc.from_spec(MOCK_OPENAPI_DICT, mode="dict")
-        html = doc.to_html()
-        self.assertIn("<!DOCTYPE html>", html)
-        self.assertIn("Sample API", html)
 
     def test_scalar_doc_from_url(self):
         docs = ScalarDoc.from_spec(
@@ -32,13 +30,13 @@ class TestScalarDoc(unittest.TestCase):
         self.assertIsNotNone(docs)
 
     def test_scalar_doc_set_spec(self):
-        doc = ScalarDoc.from_spec(MOCK_OPENAPI_DICT, mode="dict")
-        doc.set_spec(MOCK_OPENAPI_DICT, mode="dict")
+        doc = ScalarDoc.from_spec(MOCK_OPENAPI_JSON, mode="json")
+        doc.set_spec(MOCK_OPENAPI_JSON, mode="json")
         html = doc.to_html()
         self.assertIn("Sample API", html)
 
     def test_export_to_html_file(self):
-        doc = ScalarDoc.from_spec(MOCK_OPENAPI_DICT, mode="dict")
+        doc = ScalarDoc.from_spec(MOCK_OPENAPI_JSON, mode="json")
         with tempfile.TemporaryDirectory() as tmpdir:
             output_path = os.path.join(tmpdir, "docs.html")
             doc.to_file(output_path)
